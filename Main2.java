35697bdaae7ec0653b05f55d3e8031023c4b282f
Babab-23 <adrianidialu@gmail.com>
2025-11-09 20:43:29 +0100

    second commit

---
+++ Main2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main2 {
+
+    public static void main(String[] args) {
+         Scanner sc =new Scanner(System.in);
+         System.out.println("Hello User please Enter your name:");
+         String name =sc.nextLine();
+         System.out.println("Enter your lucky number:");
+         int lucky=sc.nextInt();
+         System.out.println("Hello"+name+",Your lucky number"+lucky+",shows you are really hardworking.Keep it up!");
+
+    }
+}
